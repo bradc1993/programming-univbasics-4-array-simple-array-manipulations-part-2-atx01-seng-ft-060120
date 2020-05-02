7652f63e3e48099168c9dea95458ece3dcc683ac
Brad Carter <bradc1993@gmail.com>
2020-05-02 08:40:33 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,23 @@
+def using_concat(array1, array2)
+  do array1.concat(array2)
+end
+
+def using_insert(array, element)
+  do array.insert(element)
+end
+
+def using_uniq(array)
+  do array.uniq
+end
+
+def using_flatten(array)
+  do array.flatten
+end
+
+def using_delete(array, string)
+  do array.delete(string)
+end
+
+def using_delete_at(array, integer)
+  do array.delete_at(integer)
+end
